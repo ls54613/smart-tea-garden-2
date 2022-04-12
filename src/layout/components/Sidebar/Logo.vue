42e624f175fb80559cdc9bb9b6dd41e404a61158
ls54613 <c>
2022-04-12 09:37:44 +0800

    更新

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -7,7 +7,7 @@
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <!-- <img v-if="logo" :src="logo" class="sidebar-logo" /> -->
-        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' ? variables.sidebarTitle : variables.sidebarLightTitle,width: '100%',fontSize: 'large' }">{{enterprise}}<br>{{title}}</h1>
+        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' ? variables.sidebarTitle : variables.sidebarLightTitle,width: '100%',fontSize: 'large' }"><br>{{title}}</h1>
       </router-link>
     </transition>
   </div>
